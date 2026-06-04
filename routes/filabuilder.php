bb78f48635a8dfc99dd038fc68df59fec53b16a1
Rizky Dhani <rizkydhni03@gmail.com>
2026-06-04 09:13:56 +0700

    feat: add builder GET and POST routes

--- routes/filabuilder.php
+++ routes/filabuilder.php
@@ -11,3 +11,11 @@ Route::prefix('filabuilder/api')
         Route::get('blocks', [BlockController::class, 'index'])->name('blocks');
         Route::post('page/{page}/save', [PageController::class, 'save'])->name('page.save');
     });
+
+Route::get('builder/{page}', [PageController::class, 'builder'])
+    ->middleware('web')
+    ->name('filabuilder.builder');
+
+Route::post('builder/{page}', [PageController::class, 'builderSave'])
+    ->middleware('web')
+    ->name('filabuilder.builder.save');
